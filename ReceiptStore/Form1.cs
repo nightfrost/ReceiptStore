768ed9548a2ef17605f5411a2ed78d61af07154a
nightfrost <lucas.rosenorn@outlook.dk>
2016-07-03 20:36:48 +0200

    Initial Commit

---
+++ ReceiptStore/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ReceiptStore
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = @"C:\Program Files\Receipts";
+
+            try
+            {
+                if (Directory.Exists(path))
+                {
+                    return;
+                }
+                DirectoryInfo di = Directory.CreateDirectory(path);
+
+                MessageBox.Show(string.Format("The directory was successfully created at: {0}", Directory.GetCreationTime(path).ToString()));
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void FolderCreate(string DirectoryName)
+        {
+            //string folderName = 
+        }
+    }
+}
